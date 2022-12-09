3f49822fb7a4e18d2474c2021bcdad45e3e9c329
JidapaBubu <112105242+JidapaBubu@users.noreply.github.com>
2022-12-09 14:57:56 +0700

    Update_program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> satcking = new Stack<int>();
+        whlie(true){
+            char text = char.Pharse(Console.ReadLine());
+            if (text == '(')
+            {
+                satcking.Push(2);
+            }
+            else if (text == ')')
+            {
+                satcking.Push(1);
+            }
+        }
+    }
+}
