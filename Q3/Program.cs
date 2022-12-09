272e94fd2b9ab7e4af025b6488c9a63689a1ba1c
JidapaBubu <112105242+JidapaBubu@users.noreply.github.com>
2022-12-09 16:44:05 +0700

    up

---
+++ Q3/Program.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static void Main(string[] args){
+
+        Stack<int> sk = new Stack<int>();
+        stack.Push(3);
+        stack.Push(1);
+        Console.WriteLine(stack.GetLength());
+
+
+    }
+}
